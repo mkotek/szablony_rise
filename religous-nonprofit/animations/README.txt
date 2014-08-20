922825b217a131c0ce9ca84242f552182b1a8fd5
Mathew Meiers <mlm@risedisplay.com>
2014-08-20 10:33:36 -0500

    cleanup
    got rid of old templates

--- religous-nonprofit/animations/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/GuiHarrison/pen/dbnmG.
-
- A list randomly rotating around with some cool filters on top. CSS for maditation. Or party.
--- religous-nonprofit/animations/js/index.js
+++
@@ -1,8 +0,0 @@
-// this will be called onload
- jQuery(function($) {
-// changed .hover to .each
-  $('.photo').each(function() {
-    var a = Math.random() < 0.5 ? Math.random()*-5000 : Math.random()*5000;             
-    $(this).css('transform', 'rotate(' + a + 'deg)');    
-  });
-});
