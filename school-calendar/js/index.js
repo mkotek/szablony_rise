d5bcaed57e70efc12e628a72e082d21848c17c3d
Mathew Meiers <mlm@risedisplay.com>
2014-10-20 15:19:04 -0500

    fixed animation

--- school-calendar/js/index.js
+++ school-calendar/js/index.js
@@ -1,4 +1,4 @@
 var tl = new TimelineMax({repeat:-1})
-tl.to("#background-pattern", 24, {backgroundPosition:"-900px 0px", ease:Linear.easeNone});
+tl.to("#background-pattern", 13, {backgroundPosition:"-450px 0px", ease:Linear.easeNone});
 
 
